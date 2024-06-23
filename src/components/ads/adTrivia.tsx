1018cc380bb74dbf307e63bb50db1e61d7600132
orelsprojects <orelsmail@gmail.com>
2024-06-23 15:54:29 +0300

    new try ads

--- src/components/ads/adTrivia.tsx
+++ src/components/ads/adTrivia.tsx
@@ -24,13 +24,7 @@ const AdTrivia: React.FC<AdTriviaProps> = () => {
       ></Script>
       <ins
         className="adsbygoogle"
-        style={{
-          display: "block",
-          textAlign: "center",
-          width: "100%",
-          height: "100px",
-          backgroundColor: "rgba(0, 0, 0, 1)",
-        }}
+        style="display:block; text-align:center; width:100%; height:100px; background-color:rgba(0, 0, 0, 1);"
         data-ad-layout="in-article"
         data-ad-format="fluid"
         data-ad-client="ca-pub-5564324340797928"
