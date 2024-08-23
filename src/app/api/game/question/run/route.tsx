13a37bcc8cbdb11803b2dc0502dbdb028a9265a2
orelsprojects <orelsmail@gmail.com>
2024-08-23 16:07:44 +0300

    fix api

--- src/app/api/game/question/run/route.tsx
+++ src/app/api/game/question/run/route.tsx
@@ -3,18 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = "nodejs";
 export const maxDuration = 60;
 
-export function GET(req: NextRequest) {
-  // wait for 55 seconds the nreturn. Do in in interval, and print something every 5 seconds
-  // Then return NextResponse.json({ text: "done" });
-  return new Promise(resolve => {
+export function GET(req: NextRequest): Promise<Response> {
+  return new Promise((resolve, reject) => {
     let i = 0;
-    const interval = setInterval(() => {
-      console.log(i + " seconds passed");
-      i += 1;
-      if (i === 71) {
-        clearInterval(interval);
-        resolve(NextResponse.json({ text: "done" }));
-      }
-    }, 1000);
+    try {
+      const interval = setInterval(() => {
+        console.log(i + " seconds passed");
+        i += 1;
+        if (i === 71) {
+          clearInterval(interval);
+          resolve(NextResponse.json({ text: "done" }));
+        }
+      }, 1000);
+    } catch (error: any) {
+      reject(NextResponse.json({ error: error.message }, { status: 500 }));
+    }
   });
 }
